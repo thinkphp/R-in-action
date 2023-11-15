e6522d5d133d0bc81e4b3cf7b09a696dfa904112
Adrian Statescu <mergesortv@gmail.com>
2023-11-15 10:10:05 +0200

    Update hackerRank.r

--- Standard-Deviation /hackerRank.r
+++ Standard-Deviation /hackerRank.r
@@ -1,10 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 f <- file("stdin")
+open(f)
 on.exit(close(f))
 
-T <- readLines(f)
-T <- strsplit(T, " ")
-v <- as.integer(T[[2]])
-write(mean(v), stdout())
-write(median(v), stdout())
-write(as.integer(names(which.max(table(v)))),stdout())
+lines <- readLines(f, warn = FALSE)
+
+n <- as.integer(lines[1])
+items <- lapply(strsplit(lines[2], ' '), as.integer)[[1]]
+
+mean_items <- mean(items)
+sd_items <- round(sqrt(sum((items - mean_items)^2) / length(items)), 1)
+
+write(sd_items, stdout())
